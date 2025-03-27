9cdbc3c0c22ecd84a6bb3656e006e1c42b31dfd1
LeonardoTavDev14 <contatoleonardotavdev@gmail.com>
2025-03-26 22:56:24 -0300

    feat: add style html - resetpassword

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -10,7 +10,24 @@ const sendMail = async (email, resetToken) => {
     to: email,
     subject: "Redefinição de senha",
     text: `Clique no link abaixo para redefinir sua senha`,
-    html: `<a href="${linkReset}">Redefinir Senha<a/>`,
+    html: `    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9;">
+    <h2 style="color: #333;">Redefinição de Senha</h2>
+    <p style="font-size: 16px; color: #555;">Olá,</p>
+    <p style="font-size: 16px; color: #555;">
+      Recebemos uma solicitação para redefinir sua senha. Se você não fez essa solicitação, ignore este e-mail.
+    </p>
+    <p style="text-align: center;">
+      <a href="${linkReset}" 
+        style="display: inline-block; padding: 12px 20px; font-size: 16px; color: #fff; background-color: #007bff; 
+        text-decoration: none; border-radius: 5px; font-weight: bold;">
+        Redefinir Senha
+      </a>
+    </p>
+    <p style="font-size: 14px; color: #777; margin-top: 20px;">
+      Este link é válido por 15 minutos. Caso tenha problemas, solicite novamente a mudança de senha.
+    </p>
+    <p style="font-size: 14px; color: #777;">Atenciosamente, <br> Equipe Suporte</p>
+  </div>`,
   };
 
   try {
